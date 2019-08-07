9cf19a9ed7aa85de93953b91127db3a77ba1bc10
hanpengluo <43666525+hanpengluo@users.noreply.github.com>
2019-08-07 15:29:48 +0800

    Add files via upload

---
+++ Rcode.R
@@ -0,0 +1,26 @@
+#set path
+setwd("D:\\2-test\\20190807SNP\\")
+genotype_file_name<-"test_qc.raw"
+map_file_name<-"test.map"
+out_name<-"genotype"
+plink_blupf90<-function(genotype_file_name,map_file_name,out_name){
+  if(!require(data.table)) install.packages("data.table")
+  ped<-fread(genotype_file_name,header = F)
+  map<-fread("test_qc.map",header = F)
+  #genotype for blupf90
+  ped<-ped[-1,-c(1,3:6)]
+  ped[is.na(ped)]=5
+  ped_blupf90<-matrix(nrow = nrow(ped),ncol = 2)
+  n<-max(nchar(ped[1,]))
+  for(i in 1:nrow(ped)){
+    id<-as.character(ped[i,1])
+    ped_blupf90[i,1]<-sprintf(paste("%",n,"s",sep = ""),id)
+    geno<-as.numeric(ped[i,-1])
+    ped_blupf90[i,2]<-paste(geno,collapse = "")
+  }
+  fwrite(as.data.frame(ped_blupf90),paste(out_name,"_blupf90",sep = ""),row.names = F,col.names = F,sep = " ",quote = F)
+  #map infomation for blupf90
+  map$snp_order<-seq(1:nrow(map))
+  fwrite(map[,c(5,1,4)],paste(out_name,"_map_blupf90",sep = ""),row.names = F,col.names = F,sep = " ",quote = F)
+}
+plink_blupf90(genotype_file_name = genotype_file_name,map_file_name = map_file_name,out_name = out_name)
